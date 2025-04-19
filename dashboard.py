405c2068caef54a4af52159a667937c9fadedfb8
Paskal <paskalis007stars@gmail.com>
2025-04-19 23:56:57 +0800

    first commit

---
+++ dashboard.py
@@ -0,0 +1,13 @@
+# import library
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+# persiapan data
+
+# testing
+st.write("Hello, World!")
+
